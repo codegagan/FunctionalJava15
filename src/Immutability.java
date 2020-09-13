06c1196aae6ce171e10bef3f9a11e59d4491f018
Gagandeep Singh <codegagan@gmail.com>
2020-09-13 16:41:36 +0530

    Added some code

---
+++ src/Immutability.java
@@ -0,0 +1,27 @@
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+public class Immutability {
+    public static void main(String[] args) {
+        final List<String> a = List.of("a");
+//        a.add("");
+        final Date date = new Date();
+        final LocalDateTime now = LocalDateTime.now();
+        final LocalDateTime dayBefore = now.minusDays(1);
+        System.out.println(dayBefore);
+
+
+        final int[] numbers = {1, 2, 3, 4, 5};
+        doSomethingFromNumbers(numbers);
+
+
+//        System.out.println(Arrays.toString(numbers));
+
+    }
+
+    private static void doSomethingFromNumbers(int[] numbers) {
+        numbers[3] = 77;
+    }
+}
---
+++ src/Lazy.java
@@ -0,0 +1,26 @@
+import java.util.function.Supplier;
+
+public class Lazy {
+    public static void main(String[] args) {
+        doSomethingWithValues(4, getTheValue());
+        doSomethingWithValues(4, Lazy::getTheValue);
+    }
+
+    private static int getTheValue() {
+        System.out.println("getting the value");
+        return 88;
+    }
+
+    private static void doSomethingWithValues(int i, int i1) {
+        if(i > 5 ){
+            System.out.println(i1);
+        }
+    }
+
+    private static void doSomethingWithValues(int i, Supplier<Integer> value) {
+        if(i > 5 ){
+            System.out.println(value.get());
+        }
+    }
+
+}
