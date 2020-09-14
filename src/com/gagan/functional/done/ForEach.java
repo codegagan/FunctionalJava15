cf340e66014754d0957064179b4548bafa8300a4
Gagandeep Singh <codegagan@gmail.com>
2020-09-14 10:45:55 +0530

    Foreach

---
+++ src/com/gagan/functional/done/ForEach.java
@@ -0,0 +1,11 @@
+package com.gagan.functional.done;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class ForEach {
+    public static void main(String[] args) {
+        Stream.of(1,2,3,4,5,8,9)
+                .forEach(System.out::println);
+    }
+}
